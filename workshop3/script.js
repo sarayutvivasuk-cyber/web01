ae2acad6a9abe7e2d6e0501ecf5aed096391e061
sarayutvivasuk-cyber <sarayutvivasuk@gmail.com>
2026-06-29 16:28:08 +0700

    Add files via upload

---
+++ workshop3/script.js
@@ -0,0 +1,10 @@
+for(i=1;i<5;i++){
+    document.write("<h",i,">Computer Science @NPRU </h",i,">");
+}
+
+for (i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        document.write("<img src='cat.jpg' width=60>");
+    }
+    document.write("<br>");
+}
